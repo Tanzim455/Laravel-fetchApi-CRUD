db01c236c219a28247b11852c1a9c712df83e41b
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-05-22 11:13:25 +0600

    Fetch backend in console via fetch

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,24 @@
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <h1 class="text-4xl">Hello</h1>
+    <script>
+//    
+
+
+
+let products=axios.get('http://127.0.0.1:8000/products')
+console.log(products);
+
+    </script>
+</body>
+</html>
