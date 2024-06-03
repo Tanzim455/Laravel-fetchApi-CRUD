46d3c281e260dcbc2cd04ef674f02ef30cb08b75
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-06-03 09:16:10 +0600

    Edit and delete button fixed

--- resources/views/products/viewall.blade.php
+++ resources/views/products/viewall.blade.php
@@ -55,8 +55,7 @@ $(document).ready(function () {
                        <td>' + item.id + '</td>\
                        <td>' + item.title + '</td>\
                       <td>' + item.description+ '</td>\
-                        <td><button type="button" value="' + item.id + '" class="dltButton">Delete</button></td>\
-                        <td><button type="button" value="' + item.id + '" class="editButton">Edit</button></td>\
+                      <td><button value="' + item.id + '" class="dltButton">Delete</button> <button value="' + item.id + '" class="editButton">Edit</button></td>\
                       \</tr>');
                       $('td').addClass('px-6 py-4');
                       $('.dltButton').addClass('focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900');
